1f5350c72539ebd802df1389272eef6cf3b6662c
Eugene <eugeneayonga@gmail.com>
2022-06-19 22:36:29 +0300

    commit tree e7013c33c80ea5948c1d5e7b2a000e211a474cf3

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = new Set(); 
+  for (const number of array) {
+    const complement = target - number;
+
+    if (seenNumbers.has(complement)) return true;
+
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Time complexity: o(n)
+  Space complexity: o(n)
 */
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+  check if any of the remaining numbers is the complement
+  if so, return true
+  if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+  I need to make a hasTargetSum function that checks if two numbers n\
+  in an array add up to some target number. For example, n\
+  if the array is [1,2,3,4] and the target number is 6, n\
+  I know that 2 and 4 add up to 6, so I should return true. n\
+  If I have the same array and the target is 10, no two numbers in the array n\
+  add up to 10, so I should return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +53,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
